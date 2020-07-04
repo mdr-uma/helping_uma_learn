59f5051cc2d6b7bc5d614b25eb0f60131456dc9a
Collin Jilbert <cjilbert504@gmail.com>
2020-07-04 15:17:22 -0500

    added new tests to practice

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User do 
     it "has a first name attribute" do 
-        user = User.new("Uma")
+        user = User.new("Jane", "Doe")
 
-        expect(user.name).to eq("Uma")
+        expect(user.first_name).to eq("Jane")
+    end
+
+    it "has a last name attribute" do 
+        user = User.new("Jane", "Doe")
+
+        expect(user.last_name).to eq("Doe")
+    end
+end
+
+RSpec.describe User, "#fullname" do
+    it "Concatenates the users first_name and last_name" do 
+        user = User.new("Jane", "Doe")
+
+        expect(user.fullname).to eq("Jane Doe")
     end
 end
