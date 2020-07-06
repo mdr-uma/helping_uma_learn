b41b64322ab8308ea1d3ea9294a3ec48ff348973
github username <um_2003_8@hotmail.com>
2020-07-05 21:28:18 -0400

    initialize first_name and last_name then define fullname method where concatenates first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User
 
-    attr_accessor :name
+    attr_accessor :first_name, :last_name
     
-    def initialize(name)
-        @name = name
+    def initialize(first_name, last_name)
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+    def fullname
+        @first_name + " " + @last_name
     end
 end
